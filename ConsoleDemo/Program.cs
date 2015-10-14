4d119c794c913df4b3ffe1c6ed4fef487bbde0c1
Sergey Opolko <opolkosergey@gmail.com>
2015-10-14 18:05:58 +0300

    Task1 complete

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Demo task1
+            double a, precision;
+            int power;
+
+            Console.Write("Enter A (double):");
+            var isAParsedCorrectly = double.TryParse(Console.ReadLine(),out a);
+            Console.Write("Enter power (int):");
+            var isPowerParsedCorrectly = int.TryParse(Console.ReadLine(), out power);
+            Console.Write("Enter a precision (double):");
+            var isPrecisionParsedCorrectly = double.TryParse(Console.ReadLine(), out precision);
+
+            if (isAParsedCorrectly && isPowerParsedCorrectly && isPrecisionParsedCorrectly)
+                Console.WriteLine("Result = {0}",Newton.Root(a,power,precision));
+            else Console.WriteLine("Error(s) of parsing");
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1/Newton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class Newton
+    {
+        /// <summary>
+        /// Method of calculating the root of the n-th degree among the Newton method with a given accuracy
+        /// </summary>
+        /// <param name="a">The number under the root</param>
+        /// <param name="power">The degree of root</param>
+        /// <param name="precision">Necessary accuracy of calculation</param>
+        /// <returns></returns>
+        public static double Root(double a, int power, double precision)
+        {
+            double possibleResult = 1;
+            double intermediateResult = Math.Pow(possibleResult, power);
+
+            while (Math.Abs(a - intermediateResult) > precision)
+            {
+                possibleResult = ((power - 1) * possibleResult + a / Math.Pow(possibleResult, power - 1)) / power;
+                intermediateResult = Math.Pow(possibleResult, power);
+            }
+            return possibleResult;
+        }
+    }
+}
